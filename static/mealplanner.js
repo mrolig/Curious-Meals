4b1a6e054ffd2fece269cd62436b94eb7d7e278d
Michael Rolig <michael_rolig@alumni.macalester.edu>
2011-08-28 17:43:42 -0700

    support to add/remove tags

--- static/mealplanner.js
+++ static/mealplanner.js
@@ -139,11 +139,7 @@ jQuery(function() {
          this.model.bind('all', this.render);
          this.$name = $("<input class='name ui-widget' type='text'></input>")
             .appendTo(this.el);
-         /*this.el.append(" ");
-         this.$id= $("<input type='text'></input>")
-            .appendTo(this.el);*/
          this.el.append(" ");
-         // TODO automatically save X sec. after a change
          //  grey-out Save button when already saved (hasChanged == false)
          this.$save = $("<input class='save' type='button' value='Save'></input>")
             .button({disabled: true, label: "Saved"})
@@ -181,12 +177,25 @@ jQuery(function() {
          this.el.append(" minutes<br/>Cook time: ");
          this.$cookTime = $("<input class='ui-widget' type='text'></input>")
             .appendTo(this.el);
-         this.el.append(" minutes<br/>Tags:<br/>(TODO)");
+         this.el.append(" minutes<br/>Tags:");
+         this.$tags = $("<div></div>")
+            .appendTo(this.el);
+         this.el.append("Type new tags, separated by commas<br/>");
+         this.$newTags = $("<input type='text' class='ui-widget' width='40'></input>")
+            .bind('keypress', function (evt) {
+               if (evt.which == 13) {
+                  evt.preventDefault();
+                  self.parseTags(false)
+               }
+            })
+            .appendTo(this.el);
          this.el.append("<br/>Ingredients:<br/>(TODO)");
          this.$name.focus();
       },
       render : function() {
-         //this.$id.val(this.model.id);
+         if (this.model.hasChanged()) {
+            this.$save.button({disabled : false, label: "Save"}); 
+         }
          this.$name.val(this.model.get("Name"));
          this.$type.val(this.model.get("DishType"));
          this.$prepTime.val(this.model.get("PrepTimeMinutes"));
@@ -199,15 +208,40 @@ jQuery(function() {
             else
                this.$stars.eq(i).addClass("disabled");
          }
+         this.$tags.html("");
+         var tags = this.model.get("Tags");
+         if ((!tags) || tags.length == 0)
+            this.$tags.append("[none]");
+         for (var t in tags)
+         {
+            if (t > 0)
+               this.$tags.append(", ");
+            var self = this;
+            var $tag = $("<div class='tag'></div>")
+               .text(tags[t])
+               .appendTo(this.$tags);
+            (function (tag) {
+            var $delTag = $("<span class='remove ui-icon ui-icon-close'></span>")
+               .appendTo($tag)
+               .click(function () {
+                     var tags = self.model.get("Tags");
+                     var i = tags.indexOf(tag);
+                     if (i >= 0)
+                     {
+                        tags.splice(i, 1)
+                        self.model.set({Tags:tags});
+                        self.model.change();
+                        self.onChange();
+                     }
+                     
+                  });
+            }) (tags[t]);
+         }
          return this;
       },
       save : function() {
          this.$save.button({disabled : true, text : "Saving"}); 
-         this.model.save( {"Name": this.$name.val(),
-            "DishType": this.$type.val(),
-            "PrepTimeMinutes": parseInt(this.$prepTime.val()),
-            "CookTimeMinutes": parseInt(this.$cookTime.val())
-            },
+         this.model.save( {},
             {
                error : this.saveError,
                success : this.saveSuccess
@@ -227,8 +261,69 @@ jQuery(function() {
          this.remove();
       },
       onChange : function() {
+         this.model.set({"Name": this.$name.val(),
+            "DishType": this.$type.val(),
+            "PrepTimeMinutes": parseInt(this.$prepTime.val()),
+            "CookTimeMinutes": parseInt(this.$cookTime.val())
+            });
+         this.parseTags(true)
          this.$save.button({disabled : false, label: "Save"}); 
          setTimeout(this.save, 5000);
+      },
+      parseTags : function(fromChangeHandler) {
+         var newTags = this.$newTags.val();
+         if (newTags.length == 0)
+            return
+         this.$newTags.val("");
+         var quote = -1;
+         var c = 0;
+         var nextTag = ""
+         var tags = this.model.get("Tags");
+         while (c < newTags.length)
+         {
+            if (quote >= 0)
+            {
+               if (newTags[c] == '"')
+               {
+                  if (nextTag.length > 0)
+                  {
+                     tags.push(nextTag);
+                     nextTag = "";
+                  }
+                  quote = -1;
+               }
+               else
+               {
+                  nextTag += newTags[c];
+               }
+            }
+            else if (newTags[c] == "," || newTags[c] == "\r" || newTags[c] == "\n" || newTags[c] == "\t")
+            {
+               if (nextTag.length > 0)
+               {
+                  tags.push(nextTag);
+                  nextTag = "";
+               }
+            }
+            else if (newTags[c] == '"') {
+               quote = c;
+            }
+            else if (newTags[c] == " ") {
+               if (nextTag.length > 0)
+                  nextTag += " ";
+            }
+            else
+            {
+               nextTag += newTags[c];
+            }
+            c++;
+         }
+         if (nextTag.length > 0)
+            tags.push(nextTag);
+         this.model.set({Tags:tags});
+         this.model.change();
+         if (!fromChangeHandler)
+            this.onChange();
       }
    })
    window.IngredientListView = Backbone.View.extend({
@@ -277,11 +372,7 @@ jQuery(function() {
          this.model.bind('all', this.render);
          this.$name = $("<input class='name ui-widget' type='text'></input>")
             .appendTo(this.el);
-         /*this.el.append(" ");
-         this.$id= $("<input type='text'></input>")
-            .appendTo(this.el);*/
          this.el.append(" ");
-         // TODO automatically save X sec. after a change
          //  grey-out Save button when already saved (hasChanged == false)
          this.$save = $("<input class='save' type='button' value='Save'></input>")
             .button({disabled: true, label: "Saved"})
@@ -303,25 +394,63 @@ jQuery(function() {
          this.el.append("<br/>Source: ");
          this.$source= $("<input ></input>")
             .autocomplete({source:["Animal", "Vegan", "Vegetarian"], minLength:0})
-            .change(function() {alert("change"); })
             .appendTo(this.el)
          makeCombo(this.$source);
-         // TODO tags
+         this.el.append("<br/>Tags:");
+         this.$tags = $("<div></div>")
+            .appendTo(this.el);
+         this.el.append("Type new tags, separated by commas<br/>");
+         this.$newTags = $("<input type='text' class='ui-widget' width='40'></input>")
+            .bind('keypress', function (evt) {
+               if (evt.which == 13) {
+                  evt.preventDefault();
+                  self.parseTags(false)
+               }
+            })
+            .appendTo(this.el);
          this.$name.focus();
       },
       render : function() {
-         //this.$id.val(this.model.id);
+         if (this.model.hasChanged()) {
+            this.$save.button({disabled : false, label: "Save"}); 
+         }
          this.$name.val(this.model.get("Name"));
          this.$category.val(this.model.get("Category"));
          this.$source.val(this.model.get("Source"));
+         this.$tags.html("");
+         var tags = this.model.get("Tags");
+         if ((!tags) || tags.length == 0)
+            this.$tags.append("[none]");
+         for (var t in tags)
+         {
+            if (t > 0)
+               this.$tags.append(", ");
+            var self = this;
+            var $tag = $("<div class='tag'></div>")
+               .text(tags[t])
+               .appendTo(this.$tags);
+            (function (tag) {
+            var $delTag = $("<span class='remove ui-icon ui-icon-close'></span>")
+               .appendTo($tag)
+               .click(function () {
+                     var tags = self.model.get("Tags");
+                     var i = tags.indexOf(tag);
+                     if (i >= 0)
+                     {
+                        tags.splice(i, 1)
+                        self.model.set({Tags:tags});
+                        self.model.change();
+                        self.onChange();
+                     }
+                     
+                  });
+            }) (tags[t]);
+         }
          return this;
       },
       save : function() {
          this.$save.button({disabled : true, text : "Saving"}); 
-         this.model.save( {"Name": this.$name.val(),
-            "Category": this.$category.val(),
-            "Source": this.$source.val()
-            },
+         this.model.save( {},
             {
                error : this.saveError,
                success : this.saveSuccess
@@ -341,8 +470,68 @@ jQuery(function() {
          this.remove();
       },
       onChange : function() {
+         this.model.set({"Name": this.$name.val(),
+            "Category": this.$category.val(),
+            "Source": this.$source.val()
+            });
+         this.parseTags(true)
          this.$save.button({disabled : false, label: "Save"}); 
          setTimeout(this.save, 5000);
+      },
+      parseTags : function(fromChangeHandler) {
+         var newTags = this.$newTags.val();
+         if (newTags.length == 0)
+            return
+         this.$newTags.val("");
+         var quote = -1;
+         var c = 0;
+         var nextTag = ""
+         var tags = this.model.get("Tags");
+         while (c < newTags.length)
+         {
+            if (quote >= 0)
+            {
+               if (newTags[c] == '"')
+               {
+                  if (nextTag.length > 0)
+                  {
+                     tags.push(nextTag);
+                     nextTag = "";
+                  }
+                  quote = -1;
+               }
+               else
+               {
+                  nextTag += newTags[c];
+               }
+            }
+            else if (newTags[c] == "," || newTags[c] == "\r" || newTags[c] == "\n" || newTags[c] == "\t")
+            {
+               if (nextTag.length > 0)
+               {
+                  tags.push(nextTag);
+                  nextTag = "";
+               }
+            }
+            else if (newTags[c] == '"') {
+               quote = c;
+            }
+            else if (newTags[c] == " ") {
+               if (nextTag.length > 0)
+                  nextTag += " ";
+            }
+            else
+            {
+               nextTag += newTags[c];
+            }
+            c++;
+         }
+         if (nextTag.length > 0)
+            tags.push(nextTag);
+         this.model.set({Tags:tags});
+         this.model.change();
+         if (!fromChangeHandler)
+            this.onChange();
       }
    })
    window.User = Backbone.Model.extend({
